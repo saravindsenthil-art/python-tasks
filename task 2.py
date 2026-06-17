1bfa6d725bb0a6c382b293eb0d5541ea4475234b
saravindsenthil-art <saravindsenthil@gmail.com>
2026-06-17 12:58:34 +0530

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,134 @@
+#task 1
+'''a=int(input("enter value 1"))
+b=int(input("enter value 2"))
+print(a&b)'''
+
+#task 2
+'''a=int(input("enter value 1"))
+b=int(input("enter value 2"))
+print(a|b)'''
+
+#task 3
+'''a=int(input("enter value 1"))
+b=int(input("enter value 2"))
+print(a^b)'''
+
+#task 4
+'''a=int(input("enter value 1"))
+b=int(input("enter value 2"))
+print(~a)
+print(~b)'''
+
+#task 5
+'''a= int(input("bill amount in inr:"))
+a+=(a/40)
+print("Final amount with gst:",a)'''
+
+#task 6
+'''a=int(input("weight of person 1:"))
+b=int(input("weight of person 2:"))
+c=int(input("weight of person 3:"))
+d=int(input("weight of person 4:"))
+e=int(input("weight of person 5:"))
+print("average weight:",(a+b+c+d+e)/5)'''
+
+#task 7
+'''a=int(input("enter value"))
+b=(a&1)
+if b==0:
+    print("the given value is even")
+if b==1:
+    print("the given value is odd")'''
+
+#task 8
+'''a=int(input("enter value"))
+print("square of the given number is:",a**2)'''
+
+#task 9
+'''a=int(input("enter value"))
+print("cube of the given number is:",a**3)'''
+
+#task 10
+'''a=int(input("value 1:"))
+b=int(input("value 2:"))
+a=a^b
+b=a^b
+a=a^b
+print("swapped vaule 1:",a)
+print("swapped value 2:",b)'''
+
+#task 11
+'''a=int(input("value 1:"))
+b=int(input("value 2:"))
+if a==b:
+    print("the two values are equal")
+if a!=b:
+    print("the two values are different")'''
+
+#task 12
+'''a=int(input("first number:"))
+b=int(input("second number:"))
+if a>b:
+    print("the first number is greater")
+elif a<b:
+    print("the second value is greater")
+elif a==b:
+    print("both the values are equal")'''
+
+#task 13
+'''a=int(input("enter value:"))
+if a>50:
+    print("the input value is greater than 50")
+elif a<50:
+    print("the value is less than 50")
+elif a==50:
+    print("the input value is equal to 50")'''
+
+#task 14
+'''a=int(input("enter value 1:"))
+b=int(input("enter value 2"))
+if a>=b:
+    print("value 1 is greater than or equal to value 2")
+else:
+    print("value 1 is less than value 2")'''
+
+#task 15
+'''a=int(input("value 1:"))
+b=int(input("value 2"))
+if a==b:
+    print("value 1 is equal to value 2")
+else:
+    print("value 1 is not equal to value 2")'''
+
+#task 16
+'''a=[1,2,3,4,5,6,7]
+b=int(input("enter value:"))
+if b in a:
+    print("the input value exists in the list")
+else:
+    print("the input value does not exist in the list")'''
+          
+#task 17
+'''P=int(input("initial principal:"))
+r=float(input("annual interest rate as a decimal:"))
+n=int(input("number of times that interest is compunded per year:"))
+t=int(input("the time the money is invested or borrowed for, in years:"))
+print("Final amount of money accumulated after including compund interest:",P*(1+(r/n))**(n*t))'''
+
+#task 18
+'''a=[2,3,4,5,6,7]
+b=int(input("enter value:"))
+if b in a:
+    print("the value belons to the list")
+else:
+    print("the value doesnt belong to the list")'''
+
+#task 19
+'''x=2
+y=2
+print(x is y)'''
+
+#task 20
+'''x=y
+print(x is y)'''
+    
